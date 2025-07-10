4e32357456090704bc65a9f644d6b3f0ab022684
sangeeta-nayak <97823124+sangeeta-nayak@users.noreply.github.com>
2025-07-10 20:58:41 +0530

    Add files via upload
    first commit

---
+++ CustomerLoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.customerloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerLoanApplication.class, args);
+	}
+
+}
